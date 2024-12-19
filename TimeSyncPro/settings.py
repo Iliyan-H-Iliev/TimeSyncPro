8548adcb0a28e11ee0e5d5378506d9261f0a6cf3
Iliyan Iliev <iliyan.hr.iliev@gmail.com>
2024-12-19 18:14:37 +0000

    update

--- TimeSyncPro/settings.py
+++ TimeSyncPro/settings.py
@@ -286,7 +286,7 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
-USE_S3 = os.getenv("USE_S3") == "False"
+USE_S3 = os.getenv("USE_S3") == "True"
 
 
 if USE_S3:
