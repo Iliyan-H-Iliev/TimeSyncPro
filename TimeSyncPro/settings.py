7e758856b2f6c9ccb964bb1381da8ea06b4e61bf
Iliyan Iliev <iliyan.hr.iliev@gmail.com>
2024-12-19 18:13:46 +0000

    update

--- TimeSyncPro/settings.py
+++ TimeSyncPro/settings.py
@@ -183,8 +183,8 @@ USE_I18N = os.getenv("USE_I18N") == "True"
 USE_L10N = os.getenv("USE_L10N") == "True"
 USE_TZ = os.getenv("USE_TZ") == "True"
 
-
-USE_S3 = os.getenv("USE_S3") == "True"
+#
+# USE_S3 = os.getenv("USE_S3") == "True"
 
 # if USE_S3:
 # aws settings
@@ -285,3 +285,32 @@ EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
+USE_S3 = os.getenv("USE_S3") == "False"
+
+
+if USE_S3:
+
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "eu-west-2")
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'staticfiles')
+    ]
+
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+else:
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (BASE_DIR / 'staticfiles',)
+
+    # Local media files settings
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
